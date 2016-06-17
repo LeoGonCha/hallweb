008a3260cbecaa9ffa09f560ef19808ae05c4c7e
LeoGonCha <leogoncha@gmail.com>
2016-06-17 18:09:20 -0300

    Correção configuracao para openshift

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var http = require('http')
     ,app = require('./config/express')
     db = require('./config/database');
-
-http.createServer(app).listen(8080, '127.0.0.1', function() {
+	
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+	
+http.createServer(app).listen(server_port, server_ip_address, function() {
     console.log('Servidor HALL escutando na porta: ' + this.address().port);
 });
 
