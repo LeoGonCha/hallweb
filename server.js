a7a0516c0bc693a4b7cc17b0c02347e68af7b446
LeoGonCha <leogoncha@gmail.com>
2018-03-23 10:36:05 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var http = require('http')
     ,app = require('./config/express')
     db = require('./config/database');
 	
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || 'localhost';
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 	
 http.createServer(app).listen(server_port, server_ip_address, function() {
     console.log('Servidor HALL ' + server_ip_address + ' escutando na porta: ' + this.address().port);
