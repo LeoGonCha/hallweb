b8b9e509a2ec9fb6b3ebc502a27c20e94f31f735
LeoGonCha <leogoncha@gmail.com>
2018-03-28 13:15:54 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,3 +9,20 @@ http.createServer(app).listen(server_port, server_ip_address, function() {
     console.log('Servidor HALL ' + server_ip_address + ' escutando na porta: ' + this.address().port);
 });
 
+
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host     : process.env.OPENSHIFT_MYSQL_DB_HOST,
+  user     : process.env.OPENSHIFT_MYSQL_DB_USERNAME,
+  password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
+  port     : process.env.OPENSHIFT_MYSQL_DB_PORT,
+  database : process.env.OPENSHIFT_APP_NAME
+ });
+ 
+ 
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
